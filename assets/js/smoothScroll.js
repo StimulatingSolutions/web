1704c089d5d08a9eec1c5d865519ea7207e95c25
Justin Taylor <pianojet@gmail.com>
2017-01-14 14:59:19 -0500

    refining

--- assets/js/smoothScroll.js
+++ assets/js/smoothScroll.js
@@ -1,12 +1,9 @@
 $(function(){
 
   scrollTo = function(toId) {
-    console.log("toId: "+toId);
     target = $(toId);
-    console.log("target: ");
-    console.log(target);
     $('html, body').stop().animate({
-       scrollTop: $(toId).offset().top
+       scrollTop: target.offset().top
     }, 800);
   }
 
